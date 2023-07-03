b979f2dbd9717b0ef79ccef5ec176b1ce35d6109
rakib13 <rakib.omar13@gmail.com>
2023-07-03 09:56:07 +0600

    Flash Message Add

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,15 +3,27 @@
     <Link href="/hello">Show Page</Link> &nbsp;
     <Link href="/listing">Listing Page</Link>
     <!-- <div>The page with time {{ timer }}</div> -->
+    <div v-if="page.props.flash.success" class="success">
+        {{ page.props.flash.success }}
+    </div>
     <slot>
         default
     </slot>
 </template>
 
 <script setup>
-import { Link, usePage } from '@inertiajs/vue3'
+import { Link, usePage } from '@inertiajs/vue3';
+
+// page.props.flash.success
+const page = usePage()
 // import { ref } from 'vue';
 
 // const timer = ref(0);
 // setInterval(()=> timer.value++, 1000);
 </script>
+<style scoped>
+    .success{
+        background-color: greenyellow;
+        color: black;
+    }
+</style>
