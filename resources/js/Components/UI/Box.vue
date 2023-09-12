468de62e847f986639a538e6a455d94146ef14a4
rakib13 <rakib.omar13@gmail.com>
2023-09-12 15:39:35 +0600

    Design Update in Create and tailwind

--- resources/js/Components/UI/Box.vue
+++ resources/js/Components/UI/Box.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="border border-gray-200 dark:border-gray-800 rounded-md p-4 shadow-sm dark:text-gray-300">
+        <div class="text-grey-500 font-medium">
+            <slot name="header" />
+        </div>
         <slot />
     </div>
 </template>
